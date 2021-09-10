79c3584c2526fe87d6c7eb31c3dd5b53a8072d2b
Dennis7456 <denniskiplangat.dk@gmail.com>
2021-09-10 10:25:36 +0300

    Added web template and a few changes to the app file

--- server/src/app.py
+++ server/src/app.py
@@ -1,1 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+    if __name__ == "main":
+        app.run(debug=True, host = "0.0.0.0", port=int(os.environ.get("PORT", 8080)))
